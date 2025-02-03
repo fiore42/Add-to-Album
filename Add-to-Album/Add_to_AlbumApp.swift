edbbb9e6c451f9d2d5a95df46a1a2e76ac9ab0ec
alfonso <bsh0ugba@duck.com>
2025-02-03 19:30:32 +0100

    Auto-commit: 2025-02-03 19:30:32

--- Add-to-Album/Add_to_AlbumApp.swift
+++ Add-to-Album/Add_to_AlbumApp.swift
@@ -4,6 +4,7 @@ import SwiftUI
 struct Add_to_AlbumApp: App {
     
     init() {
+        Logger.log("03 Feb 2025 - 19:30 the thumbnail view works") // ✅ First debug print
         Logger.log("🚀 App launched") // ✅ First debug print
     }
     var body: some Scene {
