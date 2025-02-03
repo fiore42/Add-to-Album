5860f1b3b8365d842aa235281c68466e57365dfa
alfonso <bsh0ugba@duck.com>
2025-02-03 22:57:36 +0100

    Auto-commit: 2025-02-03 22:57:36

--- Add-to-Album/Views/FullscreenImageView.swift
+++ Add-to-Album/Views/FullscreenImageView.swift
@@ -31,6 +31,7 @@ struct FullscreenImageView: View {
                             .scaledToFit()
                             .frame(width: geometry.size.width, height: geometry.size.height)
                             .clipped()
+                            .offset(x: -geometry.size.width) // Position off-screen to the left
                     }
                     
                     if let currentImage = currentImage {
@@ -47,12 +48,13 @@ struct FullscreenImageView: View {
                             .scaledToFit()
                             .frame(width: geometry.size.width, height: geometry.size.height)
                             .clipped()
+                            .offset(x: geometry.size.width) // Position off-screen to the right
                     }
                 }
-//                .offset(x: -CGFloat(selectedImageIndex) * geometry.size.width + dragTranslation.width)
-//                .animation(.interactiveSpring(), value: selectedImageIndex)
-                .offset(x: dragTranslation.width) // Use dragTranslation directly
-                .animation(.interactiveSpring(), value: dragTranslation) // Animate with dragTranslation
+                .offset(x: CGFloat(-selectedImageIndex) * geometry.size.width + dragTranslation.width) // Key change!
+                .animation(.interactiveSpring(), value: dragTranslation) // Animate dragTranslation
+//                .offset(x: dragTranslation.width) // Use dragTranslation directly
+//                .animation(.interactiveSpring(), value: dragTranslation) // Animate with dragTranslation
                 
                 // Black separator
                 if dragTranslation != .zero { // Only show when dragging
