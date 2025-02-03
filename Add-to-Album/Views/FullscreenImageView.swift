522c6a5775a315b7b01c47642a1234266f366ddb
alfonso <bsh0ugba@duck.com>
2025-02-03 19:54:13 +0100

    Auto-commit: 2025-02-03 19:54:13

--- Add-to-Album/Views/FullscreenImageView.swift
+++ Add-to-Album/Views/FullscreenImageView.swift
@@ -8,44 +8,52 @@ struct FullscreenImageView: View {
     private let imageManager = PHImageManager.default()
 
     @State private var currentImage: UIImage?
-    @State private var leftImage: UIImage?
-    @State private var rightImage: UIImage?
-    @State private var isSwiping = false // To control animations
+    @State private var nextImage: UIImage?
+    @State private var dragOffset: CGFloat = 0
+    @State private var isSwiping = false
 
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
 
-            if let image = currentImage {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFit()
-                    .transition(.opacity) // Smooth transition effect
+            if let currentImage = currentImage {
+                GeometryReader { geometry in
+                    ZStack {
+                        Image(uiImage: currentImage)
+                            .resizable()
+                            .scaledToFit()
+                            .offset(x: dragOffset)
+                        
+                        if let nextImage = nextImage {
+                            Image(uiImage: nextImage)
+                                .resizable()
+                                .scaledToFit()
+                                .offset(x: dragOffset > 0 ? -geometry.size.width + dragOffset : geometry.size.width + dragOffset)
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                dragOffset = value.translation.width
+                            }
+                            .onEnded { value in
+                                let threshold: CGFloat = 100
+                                if value.translation.width > threshold {
+                                    showPreviousImage()
+                                } else if value.translation.width < -threshold {
+                                    showNextImage()
+                                } else {
+                                    withAnimation(.spring()) {
+                                        dragOffset = 0
+                                    }
+                                }
+                            }
+                    )
+                }
             } else {
                 ProgressView()
                     .foregroundColor(.white)
             }
-
-            HStack {
-                if leftImage != nil {
-                    Image(uiImage: leftImage!)
-                        .resizable()
-                        .scaledToFit()
-                        .opacity(0.5) // Show next/prev images as faded previews
-                        .frame(width: 50, height: 50)
-                        .onTapGesture { showPreviousImage() }
-                }
-                Spacer()
-                if rightImage != nil {
-                    Image(uiImage: rightImage!)
-                        .resizable()
-                        .scaledToFit()
-                        .opacity(0.5)
-                        .frame(width: 50, height: 50)
-                        .onTapGesture { showNextImage() }
-                }
-            }
-            .padding()
 
             VStack {
                 HStack {
@@ -60,34 +68,18 @@ struct FullscreenImageView: View {
                 Spacer()
             }
         }
-        .gesture(
-            DragGesture()
-                .onEnded { value in
-                    let translation = value.translation.width
-                    let threshold: CGFloat = 100 // More natural swipe threshold
-
-                    if translation > threshold {
-                        showPreviousImage()
-                    } else if translation < -threshold {
-                        showNextImage()
-                    }
-                }
-        )
-        .onChange(of: selectedImageIndex) { _ in loadImages() }
         .onAppear { loadImages() }
     }
 
-    // Loads the main image and adjacent images
     private func loadImages() {
         loadImage(for: imageAssets[selectedImageIndex]) { image in
             withAnimation {
                 currentImage = image
             }
         }
-        loadAdjacentImages()
+        preloadNextImage()
     }
 
-    // Loads a high-res image for current index
     private func loadImage(for asset: PHAsset, completion: @escaping (UIImage?) -> Void) {
         let targetSize = CGSize(width: UIScreen.main.bounds.width * 2, height: UIScreen.main.bounds.height * 2)
         let options = PHImageRequestOptions()
@@ -101,37 +93,57 @@ struct FullscreenImageView: View {
         }
     }
 
-    // Loads left & right images to speed up swiping
-    private func loadAdjacentImages() {
-        let targetSize = CGSize(width: UIScreen.main.bounds.width / 2, height: UIScreen.main.bounds.height / 2)
-        let options = PHImageRequestOptions()
-        options.isNetworkAccessAllowed = true
-        options.deliveryMode = .opportunistic
-
-        if selectedImageIndex > 0 {
-            imageManager.requestImage(for: imageAssets[selectedImageIndex - 1], targetSize: targetSize, contentMode: .aspectFit, options: options) { image, _ in
-                DispatchQueue.main.async { leftImage = image }
-            }
-        } else { leftImage = nil }
-
-        if selectedImageIndex < imageAssets.count - 1 {
-            imageManager.requestImage(for: imageAssets[selectedImageIndex + 1], targetSize: targetSize, contentMode: .aspectFit, options: options) { image, _ in
-                DispatchQueue.main.async { rightImage = image }
-            }
-        } else { rightImage = nil }
+    private func preloadNextImage() {
+        guard selectedImageIndex < imageAssets.count - 1 else {
+            nextImage = nil
+            return
+        }
+        loadImage(for: imageAssets[selectedImageIndex + 1]) { image in
+            nextImage = image
+        }
     }
 
-    // Moves to the previous image
     private func showPreviousImage() {
-        guard selectedImageIndex > 0 else { return }
-        isSwiping = true
-        selectedImageIndex -= 1
+        if selectedImageIndex > 0 {
+            withAnimation(.easeInOut(duration: 0.4)) {
+                selectedImageIndex -= 1
+                swap(&currentImage, &nextImage)
+                dragOffset = UIScreen.main.bounds.width
+            }
+            preloadNextImage()
+            withAnimation(.spring()) {
+                dragOffset = 0
+            }
+        } else {
+            // Bounce effect when swiping beyond the first image
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.2)) {
+                dragOffset = 50
+            }
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.2)) {
+                dragOffset = 0
+            }
+        }
     }
 
-    // Moves to the next image
     private func showNextImage() {
-        guard selectedImageIndex < imageAssets.count - 1 else { return }
-        isSwiping = true
-        selectedImageIndex += 1
+        if selectedImageIndex < imageAssets.count - 1 {
+            withAnimation(.easeInOut(duration: 0.4)) {
+                selectedImageIndex += 1
+                swap(&currentImage, &nextImage)
+                dragOffset = -UIScreen.main.bounds.width
+            }
+            preloadNextImage()
+            withAnimation(.spring()) {
+                dragOffset = 0
+            }
+        } else {
+            // Bounce effect when swiping beyond the last image
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.5, blendDuration: 0.2)) {
+                dragOffset = -50
+            }
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.2)) {
+                dragOffset = 0
+            }
+        }
     }
 }
