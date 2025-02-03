b275ad6f979503a8d037e17eb7e298d71999850c
alfonso <bsh0ugba@duck.com>
2025-02-03 19:24:38 +0100

    Auto-commit: 2025-02-03 19:24:38

--- Add-to-Album/Views/ImageCellView.swift
+++ Add-to-Album/Views/ImageCellView.swift
@@ -1,13 +1,17 @@
 import SwiftUI
 
 struct ImageCellView: View {
-    let image: UIImage
-    
+    let image: UIImage? // Or your image type
+
     var body: some View {
-        Image(uiImage: image)
-            .resizable()
-            .scaledToFill()
-            .frame(width: 100, height: 100)
-            .clipped()
+        if let image = image {
+            Image(uiImage: image)
+                .resizable() // Important: Make the image resizable
+                .scaledToFill() // Or .scaledToFit() depending on your needs
+                .clipped() // Very important: Clip to the frame bounds
+        } else {
+            // Placeholder or loading indicator
+            ProgressView() // Example placeholder
+        }
     }
 }
