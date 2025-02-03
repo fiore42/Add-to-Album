481abb23285f31117673b3ebb7e1a8b82b348d9b
alfonso <bsh0ugba@duck.com>
2025-02-03 21:59:27 +0100

    Auto-commit: 2025-02-03 21:59:27

--- Add-to-Album/Add_to_AlbumApp.swift
+++ Add-to-Album/Add_to_AlbumApp.swift
@@ -6,6 +6,7 @@ struct Add_to_AlbumApp: App {
     init() {
         Logger.log("Project History:")
         Logger.log("1. 03 Feb 2025 - 19:30 the thumbnail view works")
+        Logger.log("2. 03 Feb 2025 - 21:59 the full screen view almost works")
         Logger.log("🚀 App launched") // ✅ First debug print
     }
     var body: some Scene {
