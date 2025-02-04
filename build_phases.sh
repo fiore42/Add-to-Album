9fd84c616c7142f596da858ee42fd51c04bb4f6a
alfonso <bsh0ugba@duck.com>
2025-02-04 10:53:27 +0100

    trying to fix the images loading very slow (1s for local images)

--- build_phases.sh
+++ build_phases.sh
@@ -3,9 +3,11 @@
 # Define your repository path
 REPO_PATH="$SRCROOT"
 
+PROJ_NAME="Add-to-Album"
+
 # Define commit message file paths
-COMMIT_MSG_FILE="$REPO_PATH/commit_message.txt"
-OLD_COMMIT_MSG_FILE="$REPO_PATH/commit_message.old"
+COMMIT_MSG_FILE="$REPO_PATH/$PROJ_NAME/commit_message.txt"
+OLD_COMMIT_MSG_FILE="$REPO_PATH/$PROJ_NAME/commit_message.old"
 
 # Default commit message
 COMMIT_MSG="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
@@ -13,31 +15,32 @@ COMMIT_MSG="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
 # Navigate to repo
 cd "$REPO_PATH" || exit 1
 
-# Determine commit message
-if [ -f "$COMMIT_MSG_FILE" ]; then
-    # Check if the file is empty (only spaces or newlines)
-    if [ -s "$COMMIT_MSG_FILE" ] && grep -q '[^[:space:]]' "$COMMIT_MSG_FILE"; then
-        # If present and not empty, check against commit_message.old
-        if [ -f "$OLD_COMMIT_MSG_FILE" ] && cmp -s "$COMMIT_MSG_FILE" "$OLD_COMMIT_MSG_FILE"; then
-            COMMIT_MSG="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
-        else
-            # Use the first line as the commit message and update commit_message.old
-            COMMIT_MSG=$(head -n 1 "$COMMIT_MSG_FILE")
-            cp "$COMMIT_MSG_FILE" "$OLD_COMMIT_MSG_FILE"
-        fi
-    fi
-fi
-
-# Check for changes
+# Check for changes before determining the commit message
 if /opt/homebrew/bin/git status --porcelain | grep -q .; then
     echo "🔄 Changes detected, committing..."
+
+    # Determine commit message only if there are changes
+    if [ -f "$COMMIT_MSG_FILE" ]; then
+        # Check if the file is empty (only spaces or newlines)
+        if [ -s "$COMMIT_MSG_FILE" ] && grep -q '[^[:space:]]' "$COMMIT_MSG_FILE"; then
+            # If present and not empty, check against commit_message.old
+            if [ -f "$OLD_COMMIT_MSG_FILE" ] && cmp -s "$COMMIT_MSG_FILE" "$OLD_COMMIT_MSG_FILE"; then
+                COMMIT_MSG="Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
+            else
+                # Use the first line as the commit message and update commit_message.old
+                COMMIT_MSG=$(head -n 1 "$COMMIT_MSG_FILE")
+                cp "$COMMIT_MSG_FILE" "$OLD_COMMIT_MSG_FILE"
+            fi
+        fi
+    fi
+
     # Add all changes
     git add .
     # Commit the changes
     git commit -m "$COMMIT_MSG"
     # Push to GitHub
     git push origin main # Change 'main' to your default branch if needed
-    echo "✅ Code successfully pushed to GitHub with message $COMMIT_MSG."
+    echo "✅ Code successfully pushed to GitHub with message: \"$COMMIT_MSG\"."
 else
     echo "✅ No changes detected. Nothing to commit."
 fi
