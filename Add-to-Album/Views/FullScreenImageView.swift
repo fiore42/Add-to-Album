48b4fde26ed8093787c764e031e880d51962b0bf
alfonso <bsh0ugba@duck.com>
2025-02-05 01:49:55 +0100

    Auto-commit: 2025-02-05 01:49:55

--- Add-to-Album/Views/FullScreenImageView.swift
+++ Add-to-Album/Views/FullScreenImageView.swift
@@ -38,72 +38,7 @@ struct FullscreenImageView: View {
     @StateObject private var imageViewModel = ImageViewModel()
     @State private var previousIndex: Int = -1
 
-//    @State private var imageLoadStates: [PHAsset: LoadingState] = [:]
     @ObservedObject var imageGridViewModel: ImageGridViewModel
-
-//   enum LoadingState {
-//        case idle, loading, success, failure
-//    }
-//
-//    var body: some View {
-//        GeometryReader { geometry in
-//            ZStack {
-//                Color.black.ignoresSafeArea()
-//
-//                TabView(selection: $selectedImageIndex) {
-//                    ForEach(imageAssets.indices, id: \.self) { index in
-//                        ZStack {
-//                            Color.black.ignoresSafeArea()
-//
-//                            if let image = imageViewModel.images[imageAssets[index]] {
-//                                Image(uiImage: image)
-//                                    .resizable()
-//                                    .scaledToFit()
-//                                    .transition(.opacity)
-//                            } else if imageLoadStates[imageAssets[index]] == .loading || imageLoadStates[imageAssets[index]] == .idle {
-//                                ProgressView()
-//                                    .scaleEffect(1.5)
-//                                    .onAppear {
-//                                        loadImage(for: imageAssets[index], targetSize: geometry.size)
-//                                    }
-//                            } else {
-//                                Image(systemName: "xmark.circle")
-//                                    .resizable()
-//                                    .scaledToFit()
-//                                    .foregroundColor(.red)
-//                            }
-//                        }
-//                        .tag(index)
-//                        .onAppear {
-//                            preloadSurroundingImages(for: index, targetSize: geometry.size)
-//                            if index == imageAssets.count - 5 && !imageGridViewModel.isLoadingBatch {
-//                                imageGridViewModel.loadNextBatch()
-//                            }
-//                        }
-//                    }
-//                }
-//                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-//                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
-//
-//                VStack {
-//                    HStack {
-//                        Button(action: { dismiss() }) {
-//                            Image(systemName: "xmark.circle.fill")
-//                                .resizable()
-//                                .frame(width: 30, height: 30)
-//                                .foregroundColor(.white)
-//                                .padding()
-//                        }
-//                        Spacer()
-//                    }
-//                    Spacer()
-//                }
-//                .padding(.top, 50)
-//                .padding(.leading)
-//                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-//            }
-//        }
-//    }
     
     var body: some View {
             GeometryReader { geometry in
@@ -127,6 +62,13 @@ struct FullscreenImageView: View {
                                             imageViewModel.loadImage(for: imageAssets[index], targetSize: geometry.size)
                                         }
                                 }
+                                // Add a vertical black separator on the right edge of each image
+                                Rectangle()
+                                    .fill(Color.black)
+                                    .frame(width: 2) // Adjust thickness
+                                    .edgesIgnoringSafeArea(.all)
+                                    .offset(x: geometry.size.width / 2 - 1) // Position at right edge
+
                             }
                             .tag(index)
                             .onAppear {
@@ -172,32 +114,4 @@ struct FullscreenImageView: View {
             imageViewModel.startCaching(assets: prefetchAssets, targetSize: targetSize)
         }
 
-//    private func loadImage(for asset: PHAsset, targetSize: CGSize) {
-//        guard imageLoadStates[asset] != .loading, imageViewModel.images[asset] == nil else { return }
-//
-//        imageLoadStates[asset] = .loading
-//
-//        let manager = PHImageManager.default()
-//        let options = PHImageRequestOptions()
-//        options.isSynchronous = false
-//        options.deliveryMode = .highQualityFormat
-//
-//        manager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options) { image, info in
-//            DispatchQueue.main.async {
-//                if let image = image {
-//                    self.imageViewModel.images[asset] = image
-//                    self.imageLoadStates[asset] = .success
-//                } else {
-//                    self.imageLoadStates[asset] = .failure
-//                }
-//            }
-//        }
-//    }
-//
-//    private func preloadSurroundingImages(for index: Int, targetSize: CGSize) {
-//        let surroundingIndices = [index - 1, index, index + 1].filter { $0 >= 0 && $0 < imageAssets.count }
-//        for i in surroundingIndices {
-//            loadImage(for: imageAssets[i], targetSize: targetSize)
-//        }
-//    }
 }
