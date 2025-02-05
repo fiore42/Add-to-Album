7f88b515f0cd1d368f723469e0d3cf9a3d68db44
alfonso <bsh0ugba@duck.com>
2025-02-05 22:25:21 +0100

    Auto-commit: 2025-02-05 22:25:21

--- Add-to-Album/ViewModels/PhotoLibraryObserver.swift
+++ Add-to-Album/ViewModels/PhotoLibraryObserver.swift
@@ -30,10 +30,39 @@ class PhotoLibraryObserver: NSObject, PHPhotoLibraryChangeObserver, ObservableOb
             AlbumUtilities.updateSelectedAlbums(photoObserverAlbums: self.albums)
          }
      }
+//    func photoLibraryDidChange(_ changeInstance: PHChange) {
+//        DispatchQueue.main.async {
+//            Logger.log("🔄 Detected Changes in Photo Library - Refreshing Albums")
+//            self.fetchAlbums() // ✅ Refresh album list when a change is detected
+//        }
+//    }
+    
+    
+//    https://stackoverflow.com/questions/42657266/phphotolibrary-photolibrarydidchange-called-multiple-times-in-swift
+    
     func photoLibraryDidChange(_ changeInstance: PHChange) {
-        DispatchQueue.main.async {
-            Logger.log("🔄 Detected Changes in Photo Library - Refreshing Albums")
-            self.fetchAlbums() // ✅ Refresh album list when a change is detected
+        guard let _fetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .any, options: nil) as? PHFetchResult<PHObject> else {
+            return
+        }
+
+        if let resultDetailChanges = changeInstance.changeDetails(for: _fetchResult) {
+            let insertedObjects = resultDetailChanges.insertedObjects
+            let removedObjects = resultDetailChanges.removedObjects
+            let changedObjects = resultDetailChanges.changedObjects.filter {
+                return changeInstance.changeDetails(for: $0)?.assetContentChanged == true
+            }
+
+            if resultDetailChanges.hasIncrementalChanges && (!insertedObjects.isEmpty || !removedObjects.isEmpty || !changedObjects.isEmpty) {
+                DispatchQueue.main.async {
+                    Logger.log("🔄 Detected Changes in Photo Library - Refreshing Albums")
+                    self.fetchAlbums() // ✅ Refresh album list when a change is detected
+                }
+            }
         }
     }
+
+    
 }
+
+
+
